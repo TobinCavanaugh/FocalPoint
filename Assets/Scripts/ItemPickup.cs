6b9576875b83b63219b58654b14e4155236bd344
Jup1t3rTobinC <tobincavanaugh@gmail.com>
2022-11-20 17:59:58 -0800

    Changes to map, ai, etc

---
+++ Assets/Scripts/ItemPickup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class ItemPickup : MonoBehaviour
+    {
+        public Vector3 rotOffset;
+        public Vector3 positionOffset;
+        public Vector3 scaleOffset = Vector3.one;
+
+        [HideInInspector]
+        public Vector3 originalScale;
+
+        private void Start()
+        {
+            originalScale = transform.localScale;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerPickerUpper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class PlayerPickerUpper : MonoBehaviour
+    {
+        public float range = 4f;
+        public LayerMask lm;
+
+        public TextMeshProUGUI tmp;
+
+        public GameObject holdingItem;
+
+        private void Update()
+        {
+            tmp.text = "";
+            
+            if (Physics.Raycast(transform.position, transform.forward, out var hit, range, lm))
+            {
+                if (hit.transform.TryGetComponent(out ItemPickup ip) && holdingItem == null)
+                {
+                    tmp.text = $"Press E or F to pick up item";
+                    
+                    if (Input.GetButtonDown("Interact"))
+                    {
+                        holdingItem = ip.gameObject;
+                        var t = holdingItem.transform;
+                        t.parent = this.transform;
+                        t.localPosition = ip.positionOffset;
+                        t.localEulerAngles = ip.rotOffset;
+                        t.localScale = ip.scaleOffset;
+                        if (t.TryGetComponent(out Rigidbody rb))
+                        {
+                            Destroy(rb);
+                        }
+                    }
+                }
+            }
+
+            if (Input.GetButtonDown("Drop"))
+            {
+                holdingItem.transform.parent = null;
+                holdingItem.AddComponent<Rigidbody>();
+                holdingItem.layer = LayerMask.NameToLayer("Item");
+                holdingItem.transform.localScale = holdingItem.GetComponent<ItemPickup>().originalScale;
+                holdingItem = null;
+            }
+
+            if (holdingItem != null)
+            {
+                tmp.text = "Press Q to drop item";
+            }
+        }
+    }
+}
